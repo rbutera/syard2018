982bbe3d284f8883a33ab72e2193c265813390b7
Unknown <jerrykress.wsj@gmail.com>
2018-03-14 11:57:01 +0000

    Tested until: Check if players have duplicated locations

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,20 +1,67 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.DOUBLE;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.SECRET;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
+import java.util.function.Consumer;
+import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
+	private List<Boolean> rounds;
+	private Graph<Integer, Transport> graph;
 
+	//Constructor
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-		// TODO
+			
+			if (rounds.isEmpty()) {
+				throw new IllegalArgumentException("Empty rounds");
+				}
+			if (graph.isEmpty()) {
+				throw new IllegalArgumentException("Empty graph");
+				}
+			this.rounds = requireNonNull(rounds);
+			this.graph = requireNonNull(graph);
+
+			if (mrX.colour != BLACK) { // or mr.colour.isDetective()
+				throw new IllegalArgumentException("MrX should be Black");
+				}
+			
+			//Check for consistency as we cannot have players with the same colour and/or location or players that are null
+			//Tests testNullMrXThrow, testAnyNullDetectiveShouldThrow, testNullDetectiveShouldThrow
+			ArrayList<PlayerConfiguration> configurations = new ArrayList<>();
+			for (PlayerConfiguration configuration : restOfTheDetectives)
+			configurations.add(requireNonNull(configuration));
+			configurations.add(0, firstDetective);
+			configurations.add(0, mrX);
+
+			//Check if players have duplicated locations
+			Set<Integer> set = new HashSet<>();
+			for (PlayerConfiguration configuration : configurations) {
+			if (set.contains(configuration.location))
+				throw new IllegalArgumentException("Duplicate location");
+			set.add(configuration.location);
+		}
 	}
+	//End of Constructor
 
 	@Override
 	public void registerSpectator(Spectator spectator) {
